e90dadec5ab5274cd62c78a618e4bdaecb4c0b00
CodeAddiction <stu.a.wilson@hotmail.co.uk>
2018-08-12 02:16:22 +0100

    Alterations to the main menu - sw

--- app/src/main/java/uk/co/reddiapps/quizimodo/MainMenu.java
+++ app/src/main/java/uk/co/reddiapps/quizimodo/MainMenu.java
@@ -7,10 +7,21 @@ import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
+    Button artButton = (Button)findViewById(R.id.topic_art_button);
+    Button historyButton = (Button)findViewById(R.id.topic_history_button);
+    Button geographyButton = (Button)findViewById(R.id.topic_geography_button);
+    Button scienceButton = (Button)findViewById(R.id.topic_science_button);
+    Button spaceButton = (Button)findViewById(R.id.topic_space_button);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+    }
+
+    public void buttonDown(){
+
 
     }
 }
