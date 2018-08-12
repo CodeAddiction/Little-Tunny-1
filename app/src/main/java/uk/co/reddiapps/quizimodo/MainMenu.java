4a09ccf2dd3ccd916618bfd34cac30a102d168e0
CodeAddiction <stu.a.wilson@hotmail.co.uk>
2018-08-12 02:03:22 +0100

    Alterations to the main menu - sw

--- app/src/main/java/uk/co/reddiapps/quizimodo/MainMenu.java
+++ app/src/main/java/uk/co/reddiapps/quizimodo/MainMenu.java
@@ -1,7 +1,9 @@
 package uk.co.reddiapps.quizimodo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
     }
 }
