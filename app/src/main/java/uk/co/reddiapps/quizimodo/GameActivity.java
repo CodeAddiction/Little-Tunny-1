f7ab20f4cbc7b66908f619ba2edf06b8053ee770
Stuart Wilson <stu.a.wilson@hotmail.co.uk>
2018-08-27 02:00:28 +0100

    All topics now generate a new GameActivity with the corresponding title.
    Both portrait and landscape modes have correct layouts.

--- app/src/main/java/uk/co/reddiapps/quizimodo/GameActivity.java
+++ app/src/main/java/uk/co/reddiapps/quizimodo/GameActivity.java
@@ -1,13 +1,23 @@
 package uk.co.reddiapps.quizimodo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        title = findViewById(R.id.topic_title_tv);
+        Intent intent = getIntent();
+        String topic_title = intent.getStringExtra("title");
+        title.setText(topic_title);
+
     }
 }
--- app/src/main/java/uk/co/reddiapps/quizimodo/MainMenu.java
+++ app/src/main/java/uk/co/reddiapps/quizimodo/MainMenu.java
@@ -1,27 +1,97 @@
 package uk.co.reddiapps.quizimodo;
 
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
-    Button artButton = (Button)findViewById(R.id.topic_art_button);
-    Button historyButton = (Button)findViewById(R.id.topic_history_button);
-    Button geographyButton = (Button)findViewById(R.id.topic_geography_button);
-    Button scienceButton = (Button)findViewById(R.id.topic_science_button);
-    Button spaceButton = (Button)findViewById(R.id.topic_space_button);
+    Button artButton;
+    Button historyButton;
+    Button geographyButton;
+    Button scienceButton;
+    Button spaceButton;
+    Button randomButton;
+    String title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+
+        artButton = findViewById(R.id.topic_art_button);
+        historyButton = findViewById(R.id.topic_history_button);
+        geographyButton = findViewById(R.id.topic_geography_button);
+        scienceButton = findViewById(R.id.topic_science_button);
+        spaceButton = findViewById(R.id.topic_space_button);
+        randomButton = findViewById(R.id.topic_random_button);
+
+        artButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent artIntent = new Intent(MainMenu.this, GameActivity.class);
+                title = "Art";
+                artIntent.putExtra("title", title);
+                startActivity(artIntent);
+
+            }
+        });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artIntent = new Intent(MainMenu.this, GameActivity.class);
+                title = "History";
+                artIntent.putExtra("title", title);
+                startActivity(artIntent);
+            }
+        });
+
+        geographyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artIntent = new Intent(MainMenu.this, GameActivity.class);
+                title = "Geography";
+                artIntent.putExtra("title", title);
+                startActivity(artIntent);
+            }
+        });
+
+        scienceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artIntent = new Intent(MainMenu.this, GameActivity.class);
+                title = "Science";
+                artIntent.putExtra("title", title);
+                startActivity(artIntent);
+            }
+        });
+
+        spaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artIntent = new Intent(MainMenu.this, GameActivity.class);
+                title = "Space";
+                artIntent.putExtra("title", title);
+                startActivity(artIntent);
+            }
+        });
+
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artIntent = new Intent(MainMenu.this, GameActivity.class);
+                title = "Random";
+                artIntent.putExtra("title", title);
+                startActivity(artIntent);
+            }
+        });
     }
 
-    public void buttonDown(){
 
-
-    }
 }
